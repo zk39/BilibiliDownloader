61ada7498d3effd73f281c5be867277d6365eead
Kun Zh <carlzh23@gmail.com>
2025-10-20 15:58:31 -0500

    updates

--- downloader.ts
+++ downloader.ts
@@ -38,23 +38,8 @@ let headers = {
 let audioArr = []
 let videoArr = []
 let dolby = []
-// curl 'https://www.bilibili.com/video/BV1yyN1eMEgj/?spm_id_from=333.337.search-card.all.click&vd_source=020f49bde169054c895cbefa73c9d6ca' \
-//   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-//   -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8' \
-//   -H 'cache-control: no-cache' \
-//   -b $'testcookie=1; buvid4=C423C8AD-EF49-BC28-6A6E-33372674F50709892-024062613-SR8SF7x5RpCnI6TCDNls8g%3D%3D; buvid_fp_plain=undefined; enable_web_push=DISABLE; DedeUserID=11261913; DedeUserID__ckMd5=4c0a6ef27f979261; PVID=4; enable_feed_channel=ENABLE; _uuid=51B103782-4595-196A-510CC-FC339D99EE3228201infoc; header_theme_version=OPEN; theme-tip-show=SHOWED; theme-avatar-tip-show=SHOWED; theme-switch-show=SHOWED; go-old-space=1; buvid3=27BA2B4D-61FB-A14B-128E-F1014127D897546748infoc; b_nut=1753994746; rpdid=|(u)|mYlJR)~0J\'u~lRl)kk~J; CURRENT_QUALITY=80; fingerprint=20377a6f592a25efc50b26acf0ec1fdd; buvid_fp=20377a6f592a25efc50b26acf0ec1fdd; bmg_af_switch=1; bmg_src_def_domain=i0.hdslb.com; SESSDATA=229771db%2C1774705383%2C7b3fb%2A91CjASvd8LYSu-NZONuGHK1twrzgJfD-P1450C7HL-qbp3zPrZPwJEhOYRlvKBc-FwOJ4SVmpUSDJlYXdXclh0MmZmWHFLX0tvRXpXZHUxNHBIenVOZWFXUTBzQzNRVlpzdlFGTENyZENmR2dKNFpMbmt1bTZIYllacTZZV3RyQV84WWFXb0txWFVRIIEC; bili_jct=c1e81537b356e50a51976abd3e9f9e02; sid=8fvslthu; home_feed_column=5; browser_resolution=1592-732; CURRENT_FNVAL=4048; bsource=search_google; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTk0MjMzNDUsImlhdCI6MTc1OTE2NDA4NSwicGx0IjotMX0.H7pDUhOuEPxQEvcI2KsVRwCjs_haCzDaeH_lgUu5bg4; bili_ticket_expires=1759423285; bp_t_offset_11261913=1118091235391700992; b_lsid=7717C5A8_19996AFA49B' \
-//   -H 'pragma: no-cache' \
-//   -H 'priority: u=0, i' \
-//   -H 'referer: https://search.bilibili.com/video?keyword=%E9%80%86%E5%90%91b%E7%AB%99%E8%A7%86%E9%A2%91playurl&from_source=webtop_search&spm_id_from=333.1007&search_source=5' \
-//   -H 'sec-ch-ua: "Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"' \
-//   -H 'sec-ch-ua-mobile: ?0' \
-//   -H 'sec-ch-ua-platform: "Windows"' \
-//   -H 'sec-fetch-dest: document' \
-//   -H 'sec-fetch-mode: navigate' \
-//   -H 'sec-fetch-site: same-origin' \
-//   -H 'sec-fetch-user: ?1' \
-//   -H 'upgrade-insecure-requests: 1' \
-//   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
+
+
 const log = console.log;
 function cli() {
 
@@ -69,6 +54,7 @@ function cli() {
 			if (trimmed === '' || trimmed === 'enter') {
 				await getHtml();
 				await readHtml();
+				await downloadAudio();
 				console.log('✅ Done! Enter another URL or q to quit.\n');
 				return cli();
 			}
@@ -82,7 +68,7 @@ function cli() {
 		});
 	} catch (error) {
 		console.error('⚠️ Error:', error);
-		console.log('Please try again.\n');
+		log('Please try again.\n');
 		return cli();
 	}
 
@@ -97,7 +83,7 @@ async function readHtml() {
 
 
 async function getHtml() {
-	const response = await axios.get('https://www.bilibili.com/video/BV1yyN1eMEgj/', {
+	const response = await axios.get('https://www.bilibili.com/video/BV18nxfzPESy/', {
 		params: {
 			'spm_id_from': '333.337.search-card.all.click',
 			'vd_source': '020f49bde169054c895cbefa73c9d6ca'
@@ -114,15 +100,29 @@ function extractJsonFromHtml(html: string): any | null {
 	if (match && match[1]) {
 		const playinfoJson = JSON.parse(match[1]);
 		//using filter for audioarr and videoarr
-		const filePath = path.join(__dirname, 'audio.json');
-		const aud = fs.readFileSync(filePath, 'utf-8');
+		const filePath = path.join(__dirname, 'playinfo.json');
+
 		audioArr = playinfoJson.data.dash.audio;
 		videoArr = playinfoJson.data.dash.video;
 		dolby = playinfoJson.data.dash.dolby;
 
-		fs.writeFileSync('./audio.json', playinfoJson.data.dash.audio, 'utf-8');
+		fs.writeFileSync('./playinfo.json', JSON.stringify({ audioArr, videoArr, dolby }, null, 4), 'utf-8');
 
-		console.log('Extracted JSON:', audioArr);
+
+
+	}
+
+}
+function downloadAudio() {
+	const len = audioArr.length;
+	if (audioArr.length === 0) {
+		log(chalk.red('No audio streams found to download.'));
+		return;
+	}
+	if (audioArr.length > 1) {
+		log(chalk.yellow(`Multiple audio streams found (${len}). Downloading the highest quality one.`));
+		// log(chalk.blue(`Audio stream details: ${JSON.stringify(audioArr, null, 4)}`));
+		audioArr.sort((a, b) => b.bandwidth - a.bandwidth);
 	}
 
 }
@@ -141,3 +141,4 @@ async function main() {
 }
 
 main();
+
